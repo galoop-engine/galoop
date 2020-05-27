ec8038ac42c13eceea0b83f3558abce942424865
AustinLynes <austinlynes94@gmail.com>
2020-05-27 12:33:03 -0700

    gotta run.. will fix later

--- src/main.py
+++ src/main.py
@@ -2,10 +2,17 @@ from window import Window
 import glfw
 
 if __name__ == "__main__":
-    window = Window()
-    while not glfw.window_should_close(window.window):
-        window.update()
+    windows = [
+         Window(window_title="Main"),   
+         Window(window_title="Logger", size=(500, 200))
+         ]
+    # make the context current
 
+    for w in windows:
+        while not glfw.window_should_close(w.window):
+            w.update()
 
 # terminate the glfw process
+
 glfw.terminate()
+
--- src/window/__init__.py
+++ src/window/__init__.py
@@ -4,7 +4,11 @@ from OpenGL.GL.shaders import compileProgram, compileShader
 import numpy as np
 import pyrr
 from PIL import Image
+
 from shape import Shape, Cube
+import sys
+
+import bimpy
 
 
 class Window:
@@ -18,13 +22,15 @@ class Window:
             glUniformMatrix4fv(shape.proj_loc, 1, GL_FALSE, projection)
 
 
-    def __init__(self):
+    def __init__(self, window_title="Galoop Engine!", pos=(400,200), size=(1280, 720)):
         # initializing glfw library
+  
+
         if not glfw.init():
             raise Exception("glfw can not be initialized!")
 
         # creating the window
-        self.window= glfw.create_window(1280, 720, "My OpenGL window", None, None)
+        self.window = glfw.create_window(size[0], size[1], window_title, None, None)
 
         # check if self.window was created
         if not self.window:
@@ -32,25 +38,38 @@ class Window:
             raise Exception("glfw self.windowcan not be created!")
 
         # set window's position
-        glfw.set_window_pos(self.window, 400, 200)
+        glfw.set_window_pos(self.window, pos[0], pos[1])
 
         # set the callback function for self.windowresize
         glfw.set_window_size_callback(self.window, self.window_resize)
 
+        # self.logger = bimpy.Context()
+        # self.logger.init(200, 200, "Logger")
         # make the context current
         glfw.make_context_current(self.window)
+        # BUG 
+        # When more than one shader is applied 
+        # only the most recent shader is applied
+
+        # initalize Logger
 
         self.shapes = [
-            Cube(texture_src="textures/test.png")
+            Cube(texture_src="textures/test.png", position=[0, 0,-4]),
+            # Cube(texture_src="textures/test2.png", position=[2, 1,-4])
             ]
 
     # the main application loop
     def update(self):
+
+
         glfw.poll_events()
 
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
 
+        
         for shape in self.shapes:
             shape.render()
 
+
         glfw.swap_buffers(self.window)
+
